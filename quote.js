b34bba127c005e9d94c6a02a9022f0f680d44c66
Dhnanjay2023 <134472271+Dhnanjay2023@users.noreply.github.com>
2023-05-24 10:42:53 +0530

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,27 @@
+let Quotes=
+    [
+        "run the code or the code will run you - by shri shri 1008",
+        "hard work pays off - by himadri khali mam",
+        "mindset is what seprates the best from the rest.- by mukesh kumar",
+        "sometimes the weight you need to lose is not on your body - jai govind",
+        "trust the timing of your life.- Mohini mishra",
+        "The purpose of our lives is to be happy. — Dalai Lama",    
+        "Life is what happens when you're busy making other plans. — John Lennon",
+        "Get busy living or get busy dying. — Stephen King",
+        "You only live once, but if you do it right, once is enough. — Mae West",
+    ];
+      function hnd(){
+      var randomQuote = Quotes[Math.floor(Math.random() * Quotes.length)]
+      let out = document.getElementById("output");
+      out.innerHTML = randomQuote;
+
+  };
+
+
+
+ 
+
+
+
+  
+
